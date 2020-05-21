9c258b250824e18a233556cf8ec1e727b74783e6
Daniel West <daniel@silverback.is>
2020-05-21 10:24:50 +0100

    Improve notes for Antoine's work next week

--- src/core/middleware.ts
+++ src/core/middleware.ts
@@ -1,17 +1,16 @@
 import { routeOption } from '../utils'
 
-export default async function routeLoaderMiddleware ({ route }) {
+// I don't know whether the $cwa property will be required, but it is injected into all contexts throughout
+// Nuxt/Vuejs components. This is the class at source /src/core/cwa.ts
+export default async function routeLoaderMiddleware ({ route, $axios, $cwa }) {
   // Disable middleware if options: { cwa: false } is set on the route
   if (routeOption(route, 'cwa', false)) {
     return
   }
   console.log('will get response from api')
-  // API_URL
-  // e.g. https://api.website.com/
+  // process.server will be true for SSR and false for Client-side
 
-  // Must use VERCEL_GITLAB_COMMIT_REF with the API url to determine which API endpoint we should be using.
-  // Where VERCEL_GITLAB_COMMIT_REF is 'dev' the endpoint would be
-  // https://dev-review.api.website.com/
+  // see /src/core/cwa.ts for axios interceptors etc.
 
   // we are using vulcain on the API so should also use
   // Preload http headers
@@ -19,13 +18,12 @@ export default async function routeLoaderMiddleware ({ route }) {
   // $axios.get('/any-path')
 
   // In version 1 we used an Axios interceptor setup which handled automatically calling the
-  // API unless a property was set in the axios options explicitly overriding the baseUrl.
-  // The axios interceptors also handled refresh tokens
+  // API unless a property was set in the axios options explicitly overriding the baseUrl
 
-  // Should we investigate cache-digest? https://github.com/dunglas/vulcain/blob/master/docs/cache.md#preventing-to-push-resources-already-in-cache-cache-digests-and-casper
-
+  // Should we investigate cache-digest for client-side requests? https://github.com/dunglas/vulcain/blob/master/docs/cache.md#preventing-to-push-resources-already-in-cache-cache-digests-and-casper
 
 
+  // Just a note for later
   // Disable middleware if no route was matched to allow 404/error page
   // const matches = []
   // const Components = getMatchedComponents(ctx.route, matches)
