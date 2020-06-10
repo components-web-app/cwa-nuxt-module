2feb2dbfe21a0ea374b136dcf797946463c718e6
Daniel West <daniel@silverback.is>
2020-06-10 14:54:18 +0100

    Feat/auto-load (#6)
    * Auto-wire routes -> common page
    
    * Disable cwa route loader for login page
    
    * Auto-wire a default layout for module
    
    * Remove demo page files no longer needed
    
    * Remove redundant files

---
+++ src/core/templates/page.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Hello Pagey-Mc-Page-Face
+    <pre>
+    {{ state }}
+    </pre>
+  </div>
+</template>
+
+<script>
+  export default {
+    auth: false,
+    layout: 'cwa-layout',
+    computed: {
+      state () {
+        return this.$store.state.resources.current
+      }
+    }
+  }
+</script>
