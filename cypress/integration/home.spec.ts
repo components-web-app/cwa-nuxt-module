44347c7deed2b728f416938ddb981931d6cb5957
Daniel West <daniel@silverback.is>
2020-08-27 21:13:34 +0100

    Fix tests

--- cypress/integration/home.spec.ts
+++ cypress/integration/home.spec.ts
@@ -1,7 +1,6 @@
 describe('Sample tests', () => {
   it('Visits index page', () => {
     cy.visit('/')
-    cy.get('.component-collection h4').should('contain', 'My custom HtmlContent component. See the HTML below. That was easy!')
     cy.get('.component-collection p').should('contain', 'Bonjour mon ami')
   })
 })
