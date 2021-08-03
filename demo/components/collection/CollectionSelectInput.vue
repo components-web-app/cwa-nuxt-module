68f076e6034c953c4a843fb288288d89708b21ae
Daniel West <daniel@silverback.is>
2021-08-03 15:45:05 +0100

    Extract query select input vue component into mixin

--- demo/components/collection/CollectionSelectInput.vue
+++ demo/components/collection/CollectionSelectInput.vue
@@ -13,58 +13,11 @@
 </template>
 
 <script lang="ts">
-import Vue, { PropType } from 'vue'
-import QueryInputMixin from '@cwa/nuxt-module/core/mixins/QueryInputMixin'
-
-interface SelectOption {
-  value: string
-  label?: string
-  queryKey?: string
-}
+import Vue from 'vue'
+import QuerySelectMixin from '@cwa/nuxt-module/core/mixins/QuerySelectMixin'
 
 export default Vue.extend({
-  mixins: [QueryInputMixin],
-  props: {
-    options: {
-      type: Array as PropType<SelectOption[]>,
-      required: true
-    },
-    defaultSelectedOptionIndex: {
-      type: Number,
-      default: 0
-    }
-  },
-  data() {
-    return {
-      selectedOption: null
-    }
-  },
-  created() {
-    let selectedOptionIndex = null
-    for (const [index, option] of this.options.entries()) {
-      const optionQueryKey = this.getQueryFieldsFromOption(option)
-      if (this.$route.query[optionQueryKey[0]] === `${option.value}`) {
-        selectedOptionIndex = index
-        break
-      }
-    }
-    if (selectedOptionIndex === null) {
-      selectedOptionIndex = this.defaultSelectedOptionIndex
-    }
-    this.selectedOption = this.options[selectedOptionIndex]
-  },
-  methods: {
-    updateSelectedOption() {
-      this.inputValue = this.selectedOption?.value || null
-      this.updateQuery(0, this.getQueryFieldsFromOption(this.selectedOption))
-    },
-    getQueryFieldsFromOption(option: SelectOption) {
-      if (!option.queryKey) {
-        return this.queryFields
-      }
-      return this.queryFields.map((field) => `${field}[${option.queryKey}]`)
-    }
-  }
+  mixins: [QuerySelectMixin]
 })
 </script>
 
