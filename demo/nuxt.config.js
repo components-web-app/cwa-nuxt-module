5d4f6460cb6513b61ef82767d880778fd219c6d3
Daniel West <daniel@silverback.is>
2022-03-31 06:06:22 +0100

    Fix moment async module load

--- demo/nuxt.config.js
+++ demo/nuxt.config.js
@@ -33,10 +33,7 @@ export default {
   css: ['~/assets/sass/main.sass'],
   buildModules: ['@nuxt/typescript-build'],
   modules: [coreModuleDist],
-  plugins: [
-    { src: '~/plugins/quill', mode: 'client' },
-    { src: '~/plugins/cypress', mode: 'client' }
-  ],
+  plugins: [{ src: '~/plugins/cypress', mode: 'client' }],
   router: {
     middleware: ['auth', 'cwa']
   },
