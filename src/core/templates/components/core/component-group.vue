fd9a0ef61555ea06c2ca3931c550165dfe650db4
silverbackdan <daniel@silverback.is>
2022-11-05 12:54:48 +0000

    Fix arrayCompare

--- src/core/templates/components/core/component-group.vue
+++ src/core/templates/components/core/component-group.vue
@@ -301,6 +301,9 @@ export default Vue.extend({
           a2 = []
         }
         let i = a1.length
+        if (i !== a2.length) {
+          return false
+        }
         while (i--) {
           if (a1[i] !== a2[i]) return false
         }
