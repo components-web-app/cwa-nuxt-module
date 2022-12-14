d150c5e436f46f767cd7ad8c65eed22a67e53d4c
silverbackdan <daniel@silverback.is>
2022-12-14 17:00:58 +0000

    Test middleware does not wait for resolve on client-side

--- src/runtime/resources/resources-manager.ts
+++ src/runtime/resources/resources-manager.ts
@@ -1,4 +1,5 @@
 import { ResourcesStore } from '../storage/stores/resources/resources-store'
+import { CwaCurrentResourceInterface } from '@cwa/nuxt-module/runtime/storage/stores/resources/state'
 
 export class ResourcesManager {
   private resourcesStoreDefinition: ResourcesStore
@@ -7,16 +8,24 @@ export class ResourcesManager {
     this.resourcesStoreDefinition = resourcesStoreDefinition
   }
 
+  /**
+   * TEMPORARY FUNCTIONS FOR DEVELOPMENT
+   */
   public get currentIds () {
     return this.resourcesStore.current.currentIds
   }
 
   public get currentResources () {
-    return this.resourcesStore.current.currentIds.reduce((obj, id) => {
+    return this.resourcesStore.current.currentIds.reduce((obj, id: string) => {
       obj[id] = this.resourcesStore.current.byId[id]
       return obj
-    }, {})
+    }, {} as {
+      [key: string]: CwaCurrentResourceInterface
+    })
   }
+  /**
+   * END OF TEMPORARY FUNCTIONS FOR DEVELOPMENT
+   */
 
   private get resourcesStore () {
     return this.resourcesStoreDefinition.useStore()
