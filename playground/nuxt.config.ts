5e4ab28f761f0c07ed1acb30b403855b050c6e6e
silverbackdan <daniel@silverback.is>
2023-03-24 20:28:37 +0000

    Remove redundant module import

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,6 +1,5 @@
 import { fileURLToPath } from 'url'
 import { defineNuxtConfig } from 'nuxt/config'
-import CwaModule from '../src/module'
 
 const API_URL = process.env.API_URL || 'https://localhost:8443'
 const API_URL_BROWSER = process.env.API_URL_BROWSER || API_URL
