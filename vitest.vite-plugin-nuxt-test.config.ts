1a5a18baad4bd87ccad85b41aa3faac58148798a
silverbackdan <daniel@silverback.is>
2023-05-10 11:07:06 +0100

    Fix coverage

---
+++ vitest.vite-plugin-nuxt-test.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import NuxtVitest from 'vite-plugin-nuxt-test'
+
+export default defineConfig({
+  plugins: [NuxtVitest()],
+  test: {
+    deps: {
+      inline: ['@nuxt/test-utils']
+    },
+    coverage: {
+      provider: 'c8',
+      include: ['src/**'],
+      exclude: ['src/templates/**', 'src/**/*.spec.ts', 'src/**/*.test.ts', 'src/**/*.d.ts'],
+      all: true
+    }
+  }
+})
