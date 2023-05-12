0e5da0d64e425e1920bee09fe5df6f0adb435177
Artur Pipchenko <art.pipchenko@gmail.com>
2023-05-13 01:32:14 +0200

    + snapshot tests for other small components

---
+++ src/runtime/templates/components/utils/ProgressBar.spec.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import ProgressBar from './ProgressBar.vue'
+
+function createWrapper (loading: boolean, percent: number) {
+  return shallowMount(ProgressBar, {
+    props: {
+      show: loading,
+      percent
+    }
+  })
+}
+
+describe('ProgressBar', () => {
+  describe('snapshot', () => {
+    test('should match snapshot IF bar is shown with specified percentage', () => {
+      const wrapper = createWrapper(true, 75)
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF bar is NOT shown', () => {
+      const wrapper = createWrapper(false, 0)
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
