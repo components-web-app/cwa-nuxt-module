4fd0f093f7e7becc409d4ba605d570263f312c40
Artur Pipchenko <art.pipchenko@gmail.com>
2023-05-13 01:32:14 +0200

    + snapshot tests for other small components

---
+++ src/runtime/templates/components/utils/alert/Info.spec.ts
@@ -0,0 +1,21 @@
+import { describe, expect, test } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import Info from './Info.vue'
+
+function createWrapper (slot: string) {
+  return shallowMount(Info, {
+    slots: {
+      default: slot
+    }
+  })
+}
+
+describe('Info', () => {
+  describe('snapshot', () => {
+    test('should match snapshot with passed slot content', () => {
+      const wrapper = createWrapper('<div>This is test</div>')
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
