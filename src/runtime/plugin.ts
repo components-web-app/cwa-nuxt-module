8a2a6d71b9674e289318479fe583858db661f6de
Artur Pipchenko <art.pipchenko@gmail.com>
2023-05-15 22:21:22 +0200

    * define pinia for test env

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,4 +1,5 @@
 import { addRouteMiddleware, defineNuxtPlugin } from '#app'
+import { createPinia, setActivePinia } from 'pinia';
 import CwaRouteMiddleware from '@cwa/nuxt-module/runtime/route-middleware'
 import Cwa from '@cwa/nuxt-module/runtime/cwa'
 import { options } from '#build/cwa-options'
@@ -7,6 +8,12 @@ export default defineNuxtPlugin({
   name: 'cwa-plugin',
   enforce: 'pre',
   setup (nuxtApp) {
+    if (process.env.NODE_ENV === 'test') {
+      const pinia = createPinia();
+      nuxtApp.vueApp.use(pinia);
+      setActivePinia(pinia);
+    }
+
     addRouteMiddleware('cwa', CwaRouteMiddleware, { global: true })
     return {
       provide: {
