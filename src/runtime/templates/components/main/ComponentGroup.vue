7d643aa6361abe4b625dc86132e259e0bd9592f4
Artur <art.pipchenko@gmail.com>
2023-05-16 00:49:43 +0200

    * new import aliases

--- src/runtime/templates/components/main/ComponentGroup.vue
+++ src/runtime/templates/components/main/ComponentGroup.vue
@@ -21,10 +21,10 @@ import { storeToRefs } from 'pinia'
 import { computed, watch } from 'vue'
 import _isEqual from 'lodash/isEqual'
 import { CwaAuthStatus } from '@cwa/nuxt3/runtime/api/auth'
-import { CwaResourceTypes, getResourceTypeFromIri } from '@cwa/nuxt3/runtime/resources/resource-utils'
 import { CwaResourceApiStatuses } from '../../../storage/stores/resources/state'
 import ResourceLoader from '../core/ResourceLoader.vue'
 import ComponentPosition from '../core/ComponentPosition.vue'
+import { CwaResourceTypes, getResourceTypeFromIri } from '@cwa/nuxt3/runtime/resources/resource-utils'
 import { useCwa } from '#imports'
 
 const $cwa = useCwa()
