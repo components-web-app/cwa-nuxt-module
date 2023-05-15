4b94a02f53c62d5b6f469cd33f618dd28d753a0d
Artur Pipchenko <art.pipchenko@gmail.com>
2023-05-15 18:05:13 +0200

    * use mockComponent util for ResourceLoader tests

--- src/runtime/templates/components/core/ResourceLoader.spec.ts
+++ src/runtime/templates/components/core/ResourceLoader.spec.ts
@@ -1,6 +1,9 @@
+// @vitest-environment nuxt
 import { describe, expect, test, vi } from 'vitest'
 import { shallowMount } from '@vue/test-utils'
 import * as nuxt from '#app'
+import { mockComponent } from 'vitest-environment-nuxt/utils'
+import { ref } from 'vue'
 import * as cwa from '../../../composables/cwaComponent'
 import ResourceLoader from './ResourceLoader.vue'
 import { CwaAuthStatus } from '@cwa/nuxt-module/runtime/api/auth'
@@ -10,6 +13,20 @@ const mockPrefix = 'TestComponent'
 const mockIri = 'testIri'
 
 function createWrapper (resource: any, status?: CwaAuthStatus) {
+  mockComponent('CwaUtilsAlertWarning', () => ({
+    name: 'CwaUtilsAlertWarning'
+  }))
+
+  mockComponent('CwaUtilsSpinner', () => ({
+    name: 'CwaUtilsSpinner',
+    props: ['show']
+  }))
+
+  mockComponent('TestComponentDummyComponent', () => ({
+    name: 'TestComponentDummyComponent',
+    props: ['iri']
+  }))
+
   // @ts-ignore
   vi.spyOn(nuxt, 'useNuxtApp').mockImplementationOnce(() => ({
     $cwa: {
@@ -22,12 +39,15 @@ function createWrapper (resource: any, status?: CwaAuthStatus) {
     }
   }))
   // @ts-ignore
-  vi.spyOn(cwa, 'useCwaResource').mockImplementationOnce(() => resource)
+  vi.spyOn(cwa, 'useCwaResource').mockImplementationOnce(() => ref(resource))
 
   return shallowMount(ResourceLoader, {
     props: {
       iri: mockIri,
       componentPrefix: mockPrefix
+    },
+    global: {
+      renderStubDefaultSlot: true
     }
   })
 }
@@ -36,18 +56,16 @@ describe('ResourceLoader', () => {
   describe('computed properties', () => {
     describe('isLoading', () => {
       test('should return true IF there is no resource', () => {
-        const wrapper = createWrapper({ value: null })
+        const wrapper = createWrapper(null)
 
         expect(wrapper.vm.isLoading).toEqual(true)
       })
 
       test('should return true IF there is no resource data AND api status EQUALS in progress', () => {
         const wrapper = createWrapper({
-          value: {
-            data: null,
-            apiState: {
-              status: CwaResourceApiStatuses.IN_PROGRESS
-            }
+          data: null,
+          apiState: {
+            status: CwaResourceApiStatuses.IN_PROGRESS
           }
         })
 
@@ -56,11 +74,9 @@ describe('ResourceLoader', () => {
 
       test('should return false IF api status DOES NOT EQUAL to in progress', () => {
         const wrapper = createWrapper({
-          value: {
-            data: null,
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS
-            }
+          data: null,
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS
           }
         })
 
@@ -69,11 +85,9 @@ describe('ResourceLoader', () => {
 
       test('should return false IF resource data is present', () => {
         const wrapper = createWrapper({
-          value: {
-            data: { test: true },
-            apiState: {
-              status: CwaResourceApiStatuses.IN_PROGRESS
-            }
+          data: { test: true },
+          apiState: {
+            status: CwaResourceApiStatuses.IN_PROGRESS
           }
         })
 
@@ -83,18 +97,16 @@ describe('ResourceLoader', () => {
 
     describe('uiComponent', () => {
       test('should return nothing IF no resource is present', () => {
-        const wrapper = createWrapper({ value: null })
+        const wrapper = createWrapper(null)
 
         expect(wrapper.vm.uiComponent).not.toBeDefined()
       })
 
       test('should return nothing IF resource data is empty', () => {
         const wrapper = createWrapper({
-          value: {
-            data: null,
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS
-            }
+          data: null,
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS
           }
         })
 
@@ -104,13 +116,11 @@ describe('ResourceLoader', () => {
       test('should return component name consisting of prefix and name from resource property', () => {
         const mockComponentName = 'Test'
         const wrapper = createWrapper({
-          value: {
-            data: {
-              uiComponent: mockComponentName
-            },
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS
-            }
+          data: {
+            uiComponent: mockComponentName
+          },
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS
           }
         })
 
@@ -120,13 +130,11 @@ describe('ResourceLoader', () => {
       test('should return component name consisting of prefix and type from resource property', () => {
         const mockType = 'MType'
         const wrapper = createWrapper({
-          value: {
-            data: {
-              '@type': mockType
-            },
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS
-            }
+          data: {
+            '@type': mockType
+          },
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS
           }
         })
 
@@ -137,11 +145,9 @@ describe('ResourceLoader', () => {
     describe('hasError', () => {
       test('should return true IF api status EQUALS to error', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.ERROR,
-              error: {}
-            }
+          apiState: {
+            status: CwaResourceApiStatuses.ERROR,
+            error: {}
           }
         })
 
@@ -150,11 +156,9 @@ describe('ResourceLoader', () => {
 
       test('should return false IF api status DOES NOT EQUAL to error', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS,
-              error: {}
-            }
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS,
+            error: {}
           }
         })
 
@@ -165,11 +169,9 @@ describe('ResourceLoader', () => {
     describe('hasSilentError', () => {
       test('should return false IF hasError property is false', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS,
-              error: {}
-            }
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS,
+            error: {}
           }
         })
 
@@ -178,12 +180,10 @@ describe('ResourceLoader', () => {
 
       test('should return false IF hasError property is true AND error status code is greater than 500', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.ERROR,
-              error: {
-                statusCode: 501
-              }
+          apiState: {
+            status: CwaResourceApiStatuses.ERROR,
+            error: {
+              statusCode: 501
             }
           }
         })
@@ -193,12 +193,10 @@ describe('ResourceLoader', () => {
 
       test('should return false IF hasError property is true AND error status code is less than 400', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.ERROR,
-              error: {
-                statusCode: 399
-              }
+          apiState: {
+            status: CwaResourceApiStatuses.ERROR,
+            error: {
+              statusCode: 399
             }
           }
         })
@@ -208,12 +206,10 @@ describe('ResourceLoader', () => {
 
       test('should return true IF hasError property is true AND error status code is within range of [400:500)', () => {
         const wrapper = createWrapper({
-          value: {
-            apiState: {
-              status: CwaResourceApiStatuses.ERROR,
-              error: {
-                statusCode: 401
-              }
+          apiState: {
+            status: CwaResourceApiStatuses.ERROR,
+            error: {
+              statusCode: 401
             }
           }
         })
@@ -226,11 +222,9 @@ describe('ResourceLoader', () => {
       test('should return value from props IF it is defined', async () => {
         const mockUiComponent = {}
         const wrapper = createWrapper({
-          value: {
-            data: null,
-            apiState: {
-              status: CwaResourceApiStatuses.SUCCESS
-            }
+          data: null,
+          apiState: {
+            status: CwaResourceApiStatuses.SUCCESS
           }
         })
 
@@ -244,13 +238,11 @@ describe('ResourceLoader', () => {
   describe('watch', () => {
     test('should NOT fetch resource IF resource data IS NOT empty, silent error occurred, user is signed in', () => {
       const wrapper = createWrapper({
-        value: {
-          data: { test: true },
-          apiState: {
-            status: CwaResourceApiStatuses.ERROR,
-            error: {
-              statusCode: 401
-            }
+        data: { test: true },
+        apiState: {
+          status: CwaResourceApiStatuses.ERROR,
+          error: {
+            statusCode: 401
           }
         }
       })
@@ -260,12 +252,10 @@ describe('ResourceLoader', () => {
 
     test('should NOT fetch resource IF resource data is empty, silent error DID NOT occur, user is signed in', () => {
       const wrapper = createWrapper({
-        value: {
-          apiState: {
-            status: CwaResourceApiStatuses.ERROR,
-            error: {
-              statusCode: 399
-            }
+        apiState: {
+          status: CwaResourceApiStatuses.ERROR,
+          error: {
+            statusCode: 399
           }
         }
       })
@@ -275,12 +265,10 @@ describe('ResourceLoader', () => {
 
     test('should NOT fetch resource IF resource data is empty, silent error occurred, user is signed out', () => {
       const wrapper = createWrapper({
-        value: {
-          apiState: {
-            status: CwaResourceApiStatuses.ERROR,
-            error: {
-              statusCode: 401
-            }
+        apiState: {
+          status: CwaResourceApiStatuses.ERROR,
+          error: {
+            statusCode: 401
           }
         }
       }, CwaAuthStatus.SIGNED_OUT)
@@ -290,12 +278,10 @@ describe('ResourceLoader', () => {
 
     test('should fetch resource IF resource data is empty, silent error occurred, user is signed in', () => {
       const wrapper = createWrapper({
-        value: {
-          apiState: {
-            status: CwaResourceApiStatuses.ERROR,
-            error: {
-              statusCode: 401
-            }
+        apiState: {
+          status: CwaResourceApiStatuses.ERROR,
+          error: {
+            statusCode: 401
           }
         }
       })
@@ -306,7 +292,53 @@ describe('ResourceLoader', () => {
 
   describe('snapshots', () => {
     test('should match snapshot IF resource is loading', () => {
-      const wrapper = createWrapper({ value: null })
+      const wrapper = createWrapper(null)
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF resource is not found', () => {
+      const wrapper = createWrapper({
+        data: null,
+        apiState: {
+          status: CwaResourceApiStatuses.SUCCESS
+        }
+      })
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF component is not found', () => {
+      const wrapper = createWrapper({
+        data: {
+          uiComponent: 'Mock'
+        },
+        apiState: {
+          status: CwaResourceApiStatuses.ERROR,
+          error: {}
+        }
+      })
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF component is rendered', async () => {
+      const wrapper = createWrapper({
+        data: {
+          uiComponent: 'DummyComponent'
+        },
+        apiState: {
+          status: CwaResourceApiStatuses.SUCCESS
+        }
+      })
+
+      await wrapper.setProps({
+        uiComponent: {
+          name: 'DummyComponent',
+          template: '<div> test </div>',
+          props: ['iri']
+        }
+      })
 
       expect(wrapper.element).toMatchSnapshot()
     })
