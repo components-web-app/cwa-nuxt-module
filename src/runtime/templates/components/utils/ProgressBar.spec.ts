5300e46f3ff7be722e0771d1f5433976938e7442
Artur <art.pipchenko@gmail.com>
2023-05-17 21:12:09 +0200

    + tests for progress bar

---
+++ src/runtime/templates/components/utils/ProgressBar.spec.ts
@@ -0,0 +1,34 @@
+// @vitest-environment nuxt
+import { describe, expect, test } from 'vitest'
+import { shallowMount } from '@vue/test-utils'
+import ProgressBar from './ProgressBar'
+
+function createWrapper (percent?: number) {
+  return shallowMount(ProgressBar, {
+    props: {
+      show: percent !== undefined,
+      percent: percent || 0
+    }
+  })
+}
+describe('ProgressBar', () => {
+  describe('snapshots', () => {
+    test('should match snapshot IF progress bar is shown', () => {
+      const wrapper = createWrapper(0)
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF progress bar is NOT shown', () => {
+      const wrapper = createWrapper()
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+
+    test('should match snapshot IF progress bar has percentage', () => {
+      const wrapper = createWrapper(75)
+
+      expect(wrapper.element).toMatchSnapshot()
+    })
+  })
+})
