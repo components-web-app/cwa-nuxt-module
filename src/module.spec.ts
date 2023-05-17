fe6b660d75767dc27b8377c2bc3173240aafac5a
silverbackdan <daniel@silverback.is>
2023-05-17 10:26:30 +0100

    Remove dependencies causing upgrade blocks

--- src/module.spec.ts
+++ src/module.spec.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath } from 'url'
-import { setup, useTestContext } from '@nuxt/test-utils'
+// import { setup, useTestContext } from '@nuxt/test-utils'
 import { describe, test, expect, vi } from 'vitest'
 import * as nuxtKit from '@nuxt/kit'
-import { NuxtModule } from '@nuxt/schema'
-import CwaModule from './module'
+// import { NuxtModule } from '@nuxt/schema'
+// import CwaModule from './module'
 
 vi.mock('@nuxt/kit', async () => {
   const module = await vi.importActual<typeof nuxtKit>('@nuxt/kit')
@@ -20,23 +20,23 @@ vi.mock('@nuxt/kit', async () => {
 })
 
 // todo: await nuxt test utils update which will probably resolve but also conflicts with nuxt-vitest right now
-describe.todo('Functional: Test modules are defined when Nuxt App is setup', async () => {
+describe.todo('Functional: Test modules are defined when Nuxt App is setup', () => {
   // @ts-ignore
-  const module: NuxtModule = CwaModule
-  await setup({
-    rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
-    nuxtConfig: {
-      modules: [
-        module
-      ],
-      cwa: {
-        apiUrl: 'https://localhost:8443',
-        apiUrlBrowser: 'https://localhost:8443'
-      }
-    },
-    server: true
-  })
-  const context = useTestContext()
+  // const module: NuxtModule = CwaModule
+  // await setup({
+  //   rootDir: fileURLToPath(new URL('../playground', import.meta.url)),
+  //   nuxtConfig: {
+  //     modules: [
+  //       module
+  //     ],
+  //     cwa: {
+  //       apiUrl: 'https://localhost:8443',
+  //       apiUrlBrowser: 'https://localhost:8443'
+  //     }
+  //   },
+  //   server: true
+  // })
+  // const context = useTestContext()
 
   test('Test module setup with correct options', () => {
     expect(nuxtKit.defineNuxtModule).toHaveBeenCalledOnce()
