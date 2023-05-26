ba3eb410a0b8d43706cdcb6eca4e9df69e39c0bf
silverbackdan <daniel@silverback.is>
2023-05-26 15:29:49 +0100

    Add comment to the todo test for mocking process.server/client

--- src/runtime/composables/process.spec.ts
+++ src/runtime/composables/process.spec.ts
@@ -12,6 +12,7 @@ describe('process composable', () => {
     })
   })
 
+  // Disabled due to nuxt-vitest bug - track issue and progress here https://github.com/danielroe/nuxt-vitest/issues/162
   test.todo('should return correct values for client/server flags IF env is server-side', () => {
     process.client = false
     process.server = true
