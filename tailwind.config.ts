f273e7bc87e19ea295089ba5e807db18cc773b26
silverbackdan <daniel@silverback.is>
2023-06-24 14:55:26 +0100

    deps update and tailwind config use typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+import { Config } from 'tailwindcss'
+
 module.exports = {
   content: ['./src/**/*.vue'],
   theme: {
     extend: {
       animation: {
-        'spin-fast': 'spin .5s linear infinite',
+        'spin-fast': 'spin .5s linear infinite'
       }
-    },
+    }
   },
   plugins: [
     require('@tailwindcss/forms')
   ],
   prefix: 'cwa-',
   important: true
-}
+} as Config
