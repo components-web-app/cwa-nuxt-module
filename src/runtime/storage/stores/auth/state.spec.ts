fafbe9ee83e812f24bde80b645f58b9d6a867010
silverbackdan <daniel@silverback.is>
2023-06-30 17:15:58 +0100

    Add auth store tests

---
+++ src/runtime/storage/stores/auth/state.spec.ts
@@ -0,0 +1,14 @@
+import { describe, expect, test } from 'vitest'
+import { reactive } from 'vue'
+import state from './state'
+
+describe('Auth State context', () => {
+  test('Initial state is correct', () => {
+    const initialState = state()
+    expect(initialState).toStrictEqual({
+      data: reactive({
+        user: undefined
+      })
+    })
+  })
+})
