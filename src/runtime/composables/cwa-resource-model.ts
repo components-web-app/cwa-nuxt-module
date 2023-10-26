ecd8312f6d2e97b7bfd6887bb142a1c9fdd12708
silverbackdan <daniel@silverback.is>
2023-10-26 17:14:00 +0100

    Add typing

--- src/runtime/composables/cwa-resource-model.ts
+++ src/runtime/composables/cwa-resource-model.ts
@@ -2,11 +2,11 @@ import { computed, ref, watch } from 'vue'
 import { debounce } from 'lodash-es'
 import { useCwa } from '#cwa/runtime/composables/cwa'
 
-export const useCwaResourceModel = (iri: string, property: string) => {
+export const useCwaResourceModel = <T>(iri: string, property: string) => {
   const $cwa = useCwa()
   const resource = $cwa.resources.getResource(iri)
-  const storeValue = computed(() => resource.value?.[property])
-  const localValue = ref<any>()
+  const storeValue = computed<T|undefined>(() => resource.value?.[property])
+  const localValue = ref<T|undefined>()
   const submitting = ref(false)
   const pendingSubmit = ref(false)
   let debounced
@@ -44,7 +44,7 @@ export const useCwaResourceModel = (iri: string, property: string) => {
       submitting,
       isBusy
     },
-    model: computed({
+    model: computed<T|undefined>({
       get () {
         return localValue.value || storeValue.value
       },
