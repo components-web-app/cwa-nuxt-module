d554679330fa723d8a764fe5481e58bcda65fcfd
silverbackdan <daniel@silverback.is>
2024-02-01 17:27:59 +0000

    Add indicator for auto-hiding notifications

--- src/runtime/templates/components/main/admin/header/_parts/Notification.vue
+++ src/runtime/templates/components/main/admin/header/_parts/Notification.vue
@@ -1,23 +1,76 @@
 <template>
-  <li class="cwa-pointer-events-auto cwa-w-full cwa-max-w-sm cwa-overflow-hidden cwa-rounded-lg cwa-dark-blur cwa-ring-2 cwa-ring-magenta">
+  <li class="cwa-pointer-events-auto cwa-w-full cwa-max-w-sm cwa-overflow-hidden cwa-rounded-lg cwa-dark-blur cwa-ring-2 cwa-ring-magenta" @mouseout="setEmitTimeout" @mouseover="clearEmitTimeout">
     <div class="cwa-p-4">
-      <div class="cwa-flex cwa-items-start">
+      <div class="cwa-flex cwa-items-start cwa-relative">
         <div class="cwa-ml-3 cwa-w-0 cwa-flex-1 cwa-pt-0.5">
           <slot />
         </div>
         <div class="cwa-ml-4 cwa-flex cwa-flex-shrink-0">
-          <button type="button" @click="$emit('clear')" class="cwa-inline-flex cwa-rounded-md cwa-text-stone-400 hover:cwa-text-white cwa-transition">
+          <button type="button" class="cwa-inline-flex cwa-rounded-md cwa-text-stone-400 hover:cwa-text-white cwa-transition" @click="$emit('clear')">
             <span class="cwa-sr-only">Close</span>
             <XMarkIcon class="cwa-h-5 cwa-w-5" aria-hidden="true" />
           </button>
         </div>
+        <svg
+          class="cwa-absolute -cwa-top-0.5 -cwa-right-0.5 countdown-icon cwa-w-6 cwa-h-6 cwa-pointer-events-none"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          xml:space="preserve"
+          :class="{ 'is-progressing': emitTimeout !== undefined }"
+        >
+          <circle
+            class="circle cwa-stroke-stone-400"
+            cx="50"
+            cy="50"
+            r="44"
+            fill="transparent"
+          />
+        </svg>
       </div>
     </div>
   </li>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { XMarkIcon } from '@heroicons/vue/20/solid'
+import { onMounted, ref } from 'vue'
 
-defineEmits(['clear'])
+const emitTimeout = ref<undefined|ReturnType<typeof setTimeout>>()
+const emit = defineEmits(['clear'])
+
+function setEmitTimeout () {
+  emitTimeout.value = setTimeout(() => {
+    emit('clear')
+  }, 6000)
+}
+
+function clearEmitTimeout () {
+  if (emitTimeout.value) {
+    clearTimeout(emitTimeout.value)
+    emitTimeout.value = undefined
+  }
+}
+
+onMounted(() => {
+  setEmitTimeout()
+})
 </script>
+
+<style>
+.countdown-icon {
+  .circle {
+    stroke-width: 12;
+    transform-origin: 50% 50% 0;
+    stroke-dasharray: 314;
+    stroke-dashoffset: 0;
+    transform: rotate(-90deg);
+    transition: .6s stroke-dashoffset linear;
+  }
+  &.is-progressing {
+    .circle {
+      transition-duration: 6s;
+      stroke-dashoffset: 314;
+    }
+  }
+}
+</style>
--- src/runtime/templates/components/main/admin/header/_parts/RequestErrors.vue
+++ src/runtime/templates/components/main/admin/header/_parts/RequestErrors.vue
@@ -1,24 +1,17 @@
 <script setup lang="ts">
+import Notification from './Notification.vue'
 import { useCwa } from '#imports'
 import { ErrorType } from '#cwa/runtime/storage/stores/error/state'
-import Notification from './Notification.vue'
-import { computed } from 'vue'
 
 const $cwa = useCwa()
 
-function removeError(timestamp: number) {
+function removeError (timestamp: number) {
   $cwa.resourcesManager.removeError(timestamp)
 }
-
-// todo: work out why when removing an index transitions do not work
-const displayErrors = computed(() =>  {
-  return $cwa.resourcesManager.errors
-})
 </script>
 
 <template>
-  <!-- Global notification live region, render this permanently at the end of the document -->
-  <div v-if="$cwa.resourcesManager.hasErrors" aria-live="assertive" class="cwa-pointer-events-none cwa-fixed cwa-inset-0 cwa-flex cwa-items-end cwa-pt-20 cwa-pb-4 cwa-py-6 sm:cwa-px-4 sm:cwa-items-start cwa-z-50">
+  <div aria-live="assertive" class="cwa-pointer-events-none cwa-fixed cwa-inset-0 cwa-flex cwa-items-end cwa-pt-20 cwa-pb-4 cwa-py-6 sm:cwa-px-4 sm:cwa-items-start cwa-z-50">
     <TransitionGroup
       tag="ul"
       class="cwa-list-none cwa-flex cwa-w-full cwa-flex-col cwa-items-center cwa-space-y-4 sm:cwa-items-end"
@@ -27,16 +20,17 @@ const displayErrors = computed(() =>  {
       enter-to-class="cwa-translate-y-0 cwa-opacity-100 sm:cwa-translate-x-0"
       leave-active-class="cwa-transition cwa-ease-in cwa-duration-100"
       leave-from-class="cwa-opacity-100"
-      leave-to-class="cwa-opacity-0"
-      appear
+      leave-to-class="cwa-translate-y-2 cwa-opacity-0 sm:cwa-translate-y-0 sm:cwa-translate-x-2"
     >
       <Notification
-        v-for="(error, index) in displayErrors"
+        v-for="(error, index) in $cwa.resourcesManager.errors"
         :key="error.timestamp"
         :data-index="index"
         @clear="() => removeError(error.timestamp)"
       >
-        <p class="cwa-text-md cwa-font-bold cwa-text-white">Oops! There was a problem.</p>
+        <p class="cwa-text-md cwa-font-bold cwa-text-white">
+          Oops! There was a problem.
+        </p>
         <ul v-if="error.type === ErrorType.VALIDATION">
           <li v-for="violation in error.violations" :key="`violation-${violation.property}-${error.timestamp}`">
             {{ violation.property }}: {{ violation.message }}
