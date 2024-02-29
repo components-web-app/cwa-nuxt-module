6d9204ef2874528c8eca2b166eae4789b6eee124
silverbackdan <daniel@silverback.is>
2024-02-29 12:08:21 +0000

    Cannot infer  type - declare

--- src/runtime/api/fetcher/cwa-fetch.ts
+++ src/runtime/api/fetcher/cwa-fetch.ts
@@ -1,8 +1,8 @@
-import { $fetch } from 'ofetch'
+import { $fetch, type $Fetch } from 'ofetch'
 
 // todo: this is just a utils export of 'fetch' we shouldn't be using a class for this.
 export default class CwaFetch {
-  public readonly fetch
+  public readonly fetch: $Fetch
 
   constructor (baseURL: string) {
     this.fetch = $fetch.create({
