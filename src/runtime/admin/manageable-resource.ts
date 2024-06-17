a3029eec0041e4b9cc862693e4971573f5b08779
silverbackdan <daniel@silverback.is>
2024-06-17 14:59:06 +0100

    Watch children for changing if resource data changes

--- src/runtime/admin/manageable-resource.ts
+++ src/runtime/admin/manageable-resource.ts
@@ -6,9 +6,9 @@ import {
 } from 'vue'
 import type {
   ComponentPublicInstance,
-  ComputedRef,
   Ref,
   WatchStopHandle
+  , WatchSource
 } from 'vue'
 import { consola } from 'consola'
 import {
@@ -111,6 +111,12 @@ export default class ManageableResource {
     // is the component that was just mounted a child of this one?
     const iris = this.$cwa.resources.findAllPublishableIris(iri)
     const childIris = this.childIris.value
+    if (this.currentIri?.value === '/_/component_positions/8d181e92-214b-4f25-9028-da35cc4596cb') {
+      console.log('listened to mounted iri', iris)
+      console.log(this.currentIri.value, 'childIris', childIris)
+      console.log(this.currentResource?.data)
+    }
+
     const iriIsChild = () => {
       for (const iri of iris) {
         if (childIris.includes(iri)) {
@@ -140,7 +146,7 @@ export default class ManageableResource {
       return this.childIrisRef
     }
 
-    const watchRefs: (Ref|ComputedRef)[] = [this.$cwa.resourcesManager.addResourceEvent]
+    const watchRefs: WatchSource[] = [this.$cwa.resourcesManager.addResourceEvent, () => this.currentResource?.data]
 
     const getChildren = () => {
       const currentIri = this.currentIri?.value
@@ -214,6 +220,9 @@ export default class ManageableResource {
         return
       }
       this.childIrisRef.value = getChildren()
+      if (this.currentIri?.value === '/_/component_positions/8d181e92-214b-4f25-9028-da35cc4596cb') {
+        console.log('updated children for position', this.childIrisRef.value)
+      }
     }, {
       immediate: true
     })
