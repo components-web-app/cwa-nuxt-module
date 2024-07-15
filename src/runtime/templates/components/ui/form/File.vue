c141e42098edecf51a01de7f6e65e71830b1e058
silverbackdan <daniel@silverback.is>
2024-07-15 17:28:17 +0100

    Show file select input

--- src/runtime/templates/components/ui/form/File.vue
+++ src/runtime/templates/components/ui/form/File.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const props = defineProps<{
   label: string,
   modelValue: string|number|undefined|null
+  accept?: string
 }>()
+
+const fileInput = ref()
 
 const emit = defineEmits(['update:modelValue'])
 
@@ -16,11 +19,26 @@ const value = computed({
     emit('update:modelValue', value)
   }
 })
+
+function showFileSelect () {
+  const clickEvent = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: false
+  })
+  fileInput.value.dispatchEvent(clickEvent)
+}
 </script>
 
 <template>
-  <div class="cwa-flex cwa-items-center">
-    <CwaUiFormButton>
+  <div class="cwa-flex cwa-items-center cwa-select-none">
+    <input
+      ref="fileInput"
+      :accept="accept"
+      class="cwa-absolute cwa-h-full cwa-w-full cwa-top-0 cwa-left-0 cwa-opacity-0 cwa-outline-0"
+      type="file"
+    >
+    <CwaUiFormButton @click="showFileSelect">
       {{ label }}
     </CwaUiFormButton>
     <span class="cwa-ml-3 cwa-text-sm cwa-cursor-pointer cwa-font-medium">
