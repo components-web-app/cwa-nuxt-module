f58a3b509e2390a3709b1777598f78745798954f
silverbackdan <daniel@silverback.is>
2024-07-29 17:26:24 +0100

    Include footer logo in cwa admi

--- src/layer/pages/_cwa.vue
+++ src/layer/pages/_cwa.vue
@@ -3,6 +3,11 @@
     <div class="cwa-bg-stone-800 cwa-text-white cwa-p-8 cwa-grow">
       <NuxtPage />
     </div>
+    <div class="cwa-text-stone-600 p-6 cwa-flex cwa-justify-center">
+      <NuxtLink to="https://cwa.rocks" target="_blank" rel="noopener">
+        <CwaLogo class="cwa-h-8 cwa-w-auto" /><span class="cwa-sr-only">Components Web App</span>
+      </NuxtLink>
+    </div>
   </div>
 </template>
 
