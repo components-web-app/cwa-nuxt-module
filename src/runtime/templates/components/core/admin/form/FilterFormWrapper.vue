4d04bc3be25ee76959ee97b79e658486aec54978
silverbackdan <daniel@silverback.is>
2024-07-31 15:51:20 +0100

    Search input UI

---
+++ src/runtime/templates/components/core/admin/form/FilterFormWrapper.vue
@@ -0,0 +1,16 @@
+<template>
+  <label>
+    <span class="cwa-block cwa-text-light/70">
+      {{ label }}
+    </span>
+    <span class="cwa-flex cwa-relative">
+      <slot />
+    </span>
+  </label>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  label: string
+}>()
+</script>
