0218081bce65bb6e300ee76585485c5a5e22bae3
silverbackdan <daniel@silverback.is>
2024-07-31 18:40:48 +0100

    Minor improvements while trying to work out why options do not disappear when changing selection

--- src/runtime/composables/cwa-select-input.ts
+++ src/runtime/composables/cwa-select-input.ts
@@ -1,4 +1,4 @@
-import { computed, watch } from 'vue'
+import { computed } from 'vue'
 import { isEqual } from 'lodash-es'
 import { defu } from 'defu'
 import type { PopperOptions } from '#cwa/runtime/types/popper'
@@ -46,10 +46,6 @@ export const useCwaSelectInput = (props: SelectInputProps, emit: (event: 'update
     }
     return isEqual(a, b)
   }
-
-  watch(selectedOption, () => {
-    console.log('selected option changed')
-  })
 
   return {
     value,
