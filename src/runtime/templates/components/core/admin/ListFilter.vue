3ae804919ddb61a0742e48ca3c1e0f5b5a4b5dc5
silverbackdan <daniel@silverback.is>
2024-07-31 15:52:03 +0100

    Search input transition colours

--- src/runtime/templates/components/core/admin/ListFilter.vue
+++ src/runtime/templates/components/core/admin/ListFilter.vue
@@ -3,8 +3,8 @@
     <div class="cwa-flex cwa-space-x-8">
       <div class="cwa-flex-grow">
         <FilterFormWrapper label="Search">
-          <input type="text" class="cwa-peer cwa-bg-light/10 focus:cwa-bg-light/15 cwa-rounded-lg cwa-border-none focus:cwa-ring-0 cwa-w-full">
-          <CwaUiIconSearchIcon class="cwa-absolute cwa-right-2.5 cwa-top-1/2 -cwa-translate-y-1/2 cwa-w-[1.2rem] cwa-text-white/50 peer-focus:cwa-text-white" />
+          <input type="text" class="cwa-transition-colors cwa-peer cwa-bg-light/10 focus:cwa-bg-light/15 cwa-rounded-lg cwa-border-none focus:cwa-ring-0 cwa-w-full">
+          <CwaUiIconSearchIcon class="cwa-absolute cwa-right-2.5 cwa-top-1/2 -cwa-translate-y-1/2 cwa-w-[1.2rem] cwa-text-white/50 peer-focus:cwa-text-white cwa-transition-colors" />
         </FilterFormWrapper>
       </div>
       <div>
