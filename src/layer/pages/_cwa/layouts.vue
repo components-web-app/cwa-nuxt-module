d62aba05dcfac1565b63c239b0b4f074339d71d2
silverbackdan <daniel@silverback.is>
2024-08-02 13:19:51 +0100

    Layout item template

--- src/layer/pages/_cwa/layouts.vue
+++ src/layer/pages/_cwa/layouts.vue
@@ -1,12 +1,31 @@
 <template>
   <ListHeading title="Layouts" />
-  <ListContent fetch-url="/_/layouts" />
+  <ListContent fetch-url="/_/layouts">
+    <template #item="data">
+      <div class="cwa-flex cwa-border-b cwa-border-b-stone-700 cwa-py-4 cwa-space-x-4 cwa-items-center">
+        <div class="cwa-grow cwa-flex cwa-flex-col cwa-space-y-1">
+          <span class="cwa-text-xl">{{ data.reference }}</span>
+          <span class="cwa-text-stone-400">UI: {{ getDisplayLayoutUi(data.uiComponent) }}</span>
+        </div>
+        <div>
+          <CwaUiFormButton>
+            <CwaUiIconCogIcon class="cwa-w-6" />
+            <span class="cwa-sr-only">Settings</span>
+          </CwaUiFormButton>
+        </div>
+      </div>
+    </template>
+  </ListContent>
 </template>
 
 <script lang="ts" setup>
 import { useHead } from '#app'
 import ListHeading from '#cwa/runtime/templates/components/core/admin/ListHeading.vue'
 import ListContent from '#cwa/runtime/templates/components/core/admin/ListContent.vue'
+
+function getDisplayLayoutUi (ui: string) {
+  return ui.replace(/CwaLayout/, '')
+}
 
 useHead({
   title: 'Layouts'
