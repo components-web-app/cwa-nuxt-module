30db5db347457c4d0325351064d7b0bffd4b2221
silverbackdan <daniel@silverback.is>
2024-08-16 11:57:58 +0100

    Add button UI for deleting a route resource without any relation

--- src/layer/pages/_cwa/routes.vue
+++ src/layer/pages/_cwa/routes.vue
@@ -4,15 +4,24 @@
   <ListContent ref="listContent" fetch-url="/_/routes">
     <template #item="data">
       <div class="cwa-flex cwa-border-b cwa-border-b-stone-700 cwa-py-4 cwa-space-x-4 cwa-items-center">
-        <div class="cwa-grow cwa-flex cwa-flex-col cwa-space-y-1">
+        <div class="cwa-grow cwa-flex cwa-flex-col cwa-space-y-1 cwa-min-w-0">
           <span class="cwa-text-xl cwa-truncate">{{ data.path }}</span>
-          <span v-if="getAssociatedIri(data)" class="cwa-truncate cwa-text-stone-400">{{ getAssociatedIri(data) }}</span>
-          <span v-else-if="data.redirect" class="cwa-truncate cwa-text-stone-400">Redirects to: {{ data.redirect }}</span>
-          <span v-else class="cwa-truncate cwa-text-red-500">This route has no association and should be deleted</span>
+          <span v-if="data.redirect || getAssociatedIri(data)" class="cwa-text-stone-400">
+            <span class="cwa-inline-flex cwa-max-w-full cwa-bg-dark cwa-p-2 cwa-font-bold cwa-space-x-2">
+              <IconRoutes v-if="data.redirect" class="cwa-w-5" />
+              <IconPages v-else class="cwa-w-5" />
+              <span class="cwa-truncate">{{ data.redirect || getAssociatedIri(data) }}</span>
+            </span>
+          </span>
+          <span v-else class="cwa-truncate cwa-text-magenta cwa-font-bold">This route has no association and should be deleted</span>
         </div>
-        <div v-if="getAssociatedIri(data)">
-          <CwaUiFormButton :to="computedItemLink(getAssociatedIri(data), '_cwa-pages', '#routes')">
-            <CwaUiIconCogIcon class="cwa-w-6" />
+        <div>
+          <CwaUiFormButton v-if="getAssociatedIri(data)" :to="computedItemLink(getAssociatedIri(data), '_cwa-pages', '#routes')">
+            <IconPages class="cwa-w-6" />
+            <span class="cwa-sr-only">Settings</span>
+          </CwaUiFormButton>
+          <CwaUiFormButton v-else-if="!data.redirect" color="blue">
+            <CwaUiIconBinIcon class="cwa-w-4 cwa-m-1" />
             <span class="cwa-sr-only">Settings</span>
           </CwaUiFormButton>
         </div>
@@ -28,6 +37,8 @@ import ListHeading from '#cwa/runtime/templates/components/core/admin/ListHeadin
 import ListContent from '#cwa/runtime/templates/components/core/admin/ListContent.vue'
 import ListFilter from '#cwa/runtime/templates/components/core/admin/ListFilter.vue'
 import { useListPage } from '#cwa/layer/pages/_cwa/composables/useListPage'
+import IconPages from '#cwa/runtime/templates/components/core/assets/IconPages.vue'
+import IconRoutes from '#cwa/runtime/templates/components/core/assets/IconRoutes.vue'
 
 const listContent = ref<InstanceType<typeof ListContent> | null>(null)
 const { computedItemLink } = useListPage(listContent)
