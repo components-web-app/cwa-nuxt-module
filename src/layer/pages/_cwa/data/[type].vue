edc04da1d421ec1c4945332b77a389170d33f7a2
silverbackdan <daniel@silverback.is>
2024-08-16 22:34:38 +0100

    Initial page data category listing

--- src/layer/pages/_cwa/data/[type].vue
+++ src/layer/pages/_cwa/data/[type].vue
@@ -1,17 +1,49 @@
 <template>
-  <ListHeading :title="pageDataClassName" />
-  <pre>{{ endpoint }}</pre>
+  <ListHeading :title="pageDataClassName" @add="goToAdd" />
+  <ListFilter :order-options="orderOptions" :search-fields="['title']" />
+  <ListContent v-if="endpoint" ref="listContent" :fetch-url="endpoint">
+    <template #item="data">
+      <div class="cwa-flex cwa-border-b cwa-border-b-stone-700 cwa-py-6 cwa-space-x-4 cwa-items-center">
+        <div class="cwa-grow cwa-flex cwa-flex-col cwa-space-y-1">
+          <div class="cwa-flex cwa-items-center cwa-space-x-3">
+            <span class="cwa-text-xl">{{ data.title }}</span><span :class="['cwa-outline', 'cwa-outline-1', 'cwa-outline-offset-2', 'cwa-w-2', 'cwa-h-2', 'cwa-rounded-full', 'cwa-outline-green', 'cwa-bg-green']" />
+          </div>
+          <span class="cwa-text-stone-400">{{ data.page }}</span>
+        </div>
+        <div class="cwa-flex cwa-space-x-2">
+          <CwaUiFormButton :to="computedItemLink(data['@id'])">
+            <CwaUiIconCogIcon class="cwa-w-6" />
+            <span class="cwa-sr-only">Settings</span>
+          </CwaUiFormButton>
+          <CwaUiFormButton @click="() => goToPage(data['@id'])">
+            <CwaUiIconEyeIcon class="cwa-h-5" />
+            <span class="cwa-sr-only">View</span>
+          </CwaUiFormButton>
+        </div>
+      </div>
+    </template>
+  </ListContent>
+  <ResourceModalOverlay @reload="triggerReload" />
 </template>
 
 <script setup lang="ts">
-import { useRoute } from '#app'
 import { computed, ref, watchEffect } from 'vue'
+import { useRouter, useRoute } from 'vue-router'
 import ListHeading from '#cwa/runtime/templates/components/core/admin/ListHeading.vue'
 import { useCwa } from '#imports'
 import { useDataList } from '#cwa/layer/pages/_cwa/composables/useDataList'
+import ListFilter from '#cwa/runtime/templates/components/core/admin/ListFilter.vue'
+import ResourceModalOverlay from '#cwa/runtime/templates/components/core/admin/ResourceModalOverlay.vue'
+import ListContent from '#cwa/runtime/templates/components/core/admin/ListContent.vue'
+import { useListPage } from '#cwa/layer/pages/_cwa/composables/useListPage'
+
+const listContent = ref<InstanceType<typeof ListContent> | null>(null)
 
 const $cwa = useCwa()
 const route = useRoute()
+const router = useRouter()
+
+const { goToAdd, triggerReload, computedItemLink } = useListPage(listContent)
 
 const dataType = computed(() => {
   const typeParam = route.params.type
@@ -37,6 +69,29 @@ const pageDataClassName = computed(() => {
   return $cwa.pageDataConfig?.[dataType.value]?.name || dataType.value
 })
 const endpoint = ref<string>()
+
+const orderOptions = [
+  {
+    label: 'New - Old',
+    value: { createdAt: 'desc' }
+  },
+  {
+    label: 'Old - New',
+    value: { createdAt: 'asc' }
+  },
+  {
+    label: 'A - Z',
+    value: { title: 'asc' }
+  },
+  {
+    label: 'Z - A',
+    value: { title: 'desc' }
+  }
+]
+
+function goToPage (page: string) {
+  router.push(page)
+}
 
 // to force the loading of api documentation
 useDataList()
