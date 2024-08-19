e72f3f670bb1778a0d95b1e75f4efd3e2eac0c1f
silverbackdan <daniel@silverback.is>
2024-08-19 13:45:22 +0100

    Fix UI for routes with no relation

--- src/runtime/templates/components/core/admin/RouteListRow.vue
+++ src/runtime/templates/components/core/admin/RouteListRow.vue
@@ -2,7 +2,7 @@
   <div class="cwa-flex cwa-border-b cwa-border-b-stone-700 cwa-py-6 cwa-space-x-4 cwa-items-center">
     <div class="cwa-grow cwa-flex cwa-flex-col cwa-space-y-1 cwa-min-w-0">
       <span class="cwa-text-xl cwa-truncate">{{ data.path }}</span>
-      <span v-if="data.redirect || getAssociatedIri(data)" class="cwa-text-stone-400">
+      <span v-if="relatedResource" class="cwa-text-stone-400">
         <span class="cwa-inline-flex cwa-max-w-full cwa-bg-dark cwa-p-2 cwa-font-bold cwa-space-x-2 cwa-items-center">
           <IconRoutes v-if="resourceType === 'Route'" class="cwa-h-5" />
           <IconData v-else-if="resourceType === 'PageData'" class="cwa-h-5" />
@@ -17,12 +17,12 @@
       </span>
     </div>
     <div>
-      <CwaUiFormButton v-if="getAssociatedIri(data)" :to="linkFn(getAssociatedIri(data), '_cwa-pages', '#routes')">
+      <CwaUiFormButton v-if="relatedResource" :to="linkFn(getAssociatedIri(data), '_cwa-pages', '#routes')">
         <IconPages v-if="data.page" class="cwa-h-6" />
         <IconData v-else class="cwa-h-6" />
         <span class="cwa-sr-only">View</span>
       </CwaUiFormButton>
-      <CwaUiFormButton v-else-if="!data.redirect" color="error" @click="$emit('deleteRoute', data['@id'])">
+      <CwaUiFormButton v-else-if="!data.redirect" color="error" @click="$emit('delete', data['@id'])">
         <CwaUiIconBinIcon class="cwa-w-4 cwa-m-1" />
         <span class="cwa-sr-only">Delete</span>
       </CwaUiFormButton>
@@ -44,7 +44,7 @@ const props = defineProps<{
 }>()
 
 defineEmits<{
-  deleteRoute: [string]
+  delete: [string]
 }>()
 
 const resourceType = computed<undefined|'Route'|'Page'|'PageData'>(() => {
