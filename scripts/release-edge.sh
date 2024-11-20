f24ac1234461060d3c22eed56e9d4b5ff4a87883
silverbackdan <daniel@silverback.is>
2024-11-20 20:20:31 +0000

    Attempt workflow with npm-otp-publish for entering the OTP

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -8,8 +8,8 @@ set -xe
 node ./scripts/bump-edge.mjs
 
 # Update token
-if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+if [[ ! -z ${NPM_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
   npm whoami
