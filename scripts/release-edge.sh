c2687110f5b4bf6a85b5da7eebebbf6e2187c461
silverbackdan <daniel@silverback.is>
2025-05-21 11:14:33 +0100

    Try turning off git checks with code into pnpm-workspace.yaml

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -6,17 +6,17 @@ set -xe
 
 TAG=${1:-latest}
 
-# Bump versions to edge
-node ./scripts/bump-edge.mjs
+# turn off git checks
+echo "gitChecks: false" >> ~/pnpm-workspace.yaml
 
 # Update token
 if [[ ! -z ${NPM_AUTH_TOKEN} ]] ; then
   echo "//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-  pnpm whoami
 fi
 
-echo "gitChecks: false" >> ~/pnpm-workspace.yaml
+# Bump versions to edge
+node ./scripts/bump-edge.mjs
 
 # Release packages
 echo "Publishing package..."
