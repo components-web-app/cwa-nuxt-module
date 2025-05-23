49bbc939a26d4c17bfe74c5a07f103c479cdd2b4
silverbackdan <daniel@silverback.is>
2025-05-23 18:24:08 +0100

    Create clean npm script which will process each version to try and unpublish. Part processed

--- scripts/clean-npm-versions.sh
+++ scripts/clean-npm-versions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+set -xe
+
 VERSIONS=$(pnpm view @cwa/nuxt-edge versions --json)
 
 for id in $(echo "${VERSIONS}" | jq -r '.[]'); do
-  echo "Unpublishing @cwa/nuxt-edge@${id}"
-  read -p "Please enter the otp for NPM: " otp
-  pnpm unpublish "@cwa/nuxt-edge@${id}" --otp=$otp
+  pnpm unpublish "@cwa/nuxt-edge@${id}"
 done
