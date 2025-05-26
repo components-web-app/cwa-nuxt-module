67e3dfa16fb79352a0c39684d1ccfc7dc937b07b
silverbackdan <daniel@silverback.is>
2025-05-26 20:05:16 +0100

    Use CwaLink

--- playground/cwa/components/NavigationLink/NavigationLink.vue
+++ playground/cwa/components/NavigationLink/NavigationLink.vue
@@ -3,6 +3,7 @@
     :to="resource?.data?.url || '#'"
     exact-active-class="text-white!"
     class="md:text-sm font-medium text-white/80 transition no-underline tracking-wide hover:opacity-100 hover:text-gurkha-200"
+    :no-prefetch="undefined"
   >
     {{ resource?.data?.label || 'No Link Label' }}
   </CwaLink>
