e88e4760ed75185065cbda8caefa6953ae640bfd
silverbackdan <daniel@silverback.is>
2025-05-28 18:48:41 +0100

    Fix highlight component bug when reordering

--- src/runtime/templates/components/main/admin/resource-manager/ComponentFocus.vue
+++ src/runtime/templates/components/main/admin/resource-manager/ComponentFocus.vue
@@ -2,6 +2,7 @@
 import { computed, onBeforeUnmount, onMounted, ref, toRef, useTemplateRef, watch } from 'vue'
 import type { ComputedRef, Ref } from 'vue'
 import { useElementSize, useWindowSize } from '@vueuse/core'
+import { v4 as uuidv4 } from 'uuid'
 import { useCwa } from '#imports'
 import { getPublishedResourceState } from '#cwa/runtime/resources/resource-utils'
 
@@ -16,6 +17,7 @@ const iri = toRef(props, 'iri')
 const canvas = useTemplateRef<HTMLCanvasElement | undefined>('canvas')
 const windowSize = useWindowSize()
 const elementSizeInstances = ref<ReturnType<typeof useElementSize>[]>([])
+const reorderId = ref<string>()
 
 watch(props.domElements, (newDomElements) => {
   for (const esInstance of elementSizeInstances.value) {
@@ -46,8 +48,6 @@ const resource = computed(() => {
   return $cwa.resources.getResource(iri.value).value
 })
 
-const resourceData = computed(() => resource.value?.data)
-
 const position = computed((): {
   top: number
   left: number
@@ -64,6 +64,7 @@ const position = computed((): {
       height: windowSize.height.value,
     },
     totalWidthAndHeight: totalWidthAndHeight.value,
+    reorderId: reorderId.value,
   }
 
   for (const domElement of domElements.value) {
@@ -149,15 +150,20 @@ function drawRoundedRect(ctx: CanvasRenderingContext2D, x: number, y: number, wi
   ctx.arcTo(x, y, x, y + radius, radius)
 }
 
+function assignReorderId() {
+  reorderId.value = uuidv4()
+}
+
 onMounted(() => {
   $cwa.admin.eventBus.on('redrawFocus', redraw)
-  watch(resourceData, $cwa.admin.emitRedraw, { deep: true, flush: 'post' })
+  $cwa.admin.eventBus.on('reorder', assignReorderId)
+  watch([totalWidthAndHeight, position], $cwa.admin.emitRedraw)
   watch(canvas, newCanvas => newCanvas && redraw())
-  watch(totalWidthAndHeight, $cwa.admin.emitRedraw)
 })
 
 onBeforeUnmount(() => {
   $cwa.admin.eventBus.off('redrawFocus', redraw)
+  $cwa.admin.eventBus.off('reorder', assignReorderId)
 })
 
 defineExpose({
