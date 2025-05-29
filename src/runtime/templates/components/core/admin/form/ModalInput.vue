ad3649a884b0a167dbf421a55a8c0b96275af077
silverbackdan <daniel@silverback.is>
2025-05-29 20:24:00 +0100

    Example settings page without functionality

--- src/runtime/templates/components/core/admin/form/ModalInput.vue
+++ src/runtime/templates/components/core/admin/form/ModalInput.vue
@@ -6,7 +6,18 @@
     >
       {{ label }}
     </label>
+    <textarea
+      v-if="type === 'textarea'"
+      :id="id"
+      ref="textarea"
+      v-model="input"
+      :placeholder="placeholder"
+      :autocomplete="autocomplete"
+      rows="3"
+      class="cwa:min-h-[150px] cwa-textarea cwa:[&::-webkit-scrollbar]:hidden cwa:flex cwa:relative cwa:px-4 cwa:pt-6 cwa:pb-0 cwa:text-left cwa:text-light cwa:w-full cwa:border-0 cwa:items-center cwa:bg-transparent cwa:border-0 cwa:focus:ring-0 cwa:outline-0 cwa:resize-none"
+    />
     <input
+      v-else
       :id="id"
       v-model="model"
       :type="type"
@@ -19,8 +30,21 @@
 
 <script lang="ts" setup>
 import { useId } from 'vue'
+import { useTextareaAutosize } from '@vueuse/core'
 
 const id = useId()
 const model = defineModel<string>()
-defineProps<{ label: string, type?: 'text' | 'number' | 'email' | 'password', placeholder?: string, autocomplete?: string }>()
+const { type } = defineProps<{ label: string, type?: 'text' | 'number' | 'email' | 'password' | 'textarea', placeholder?: string, autocomplete?: string }>()
+
+const { textarea, input } = useTextareaAutosize({
+  // @ts-expect-error MaybeRef expected instead of ModelRef but they should behave the same
+  input: model,
+})
 </script>
+
+<style>
+textarea.cwa-textarea {
+  -ms-overflow-style: none;  /* IE and Edge */
+  scrollbar-width: none;  /* Firefox */
+}
+</style>
