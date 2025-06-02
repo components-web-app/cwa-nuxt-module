9986d8ad63cb60af63acd574285cef56ac2edc0e
silverbackdan <daniel@silverback.is>
2025-06-02 19:46:29 +0100

    Respect configuration in sitemap data source

--- src/runtime/server/server-middleware.ts
+++ src/runtime/server/server-middleware.ts
@@ -1,20 +1,12 @@
 import { defineEventHandler } from 'h3'
-import { consola } from 'consola'
-import type { CwaResource } from '#cwa/runtime/resources/resource-utils'
 import useCwaSiteConfig from '#cwa/runtime/composables/useCwaSiteConfig'
 import { updateSiteConfig } from '#site-config/server/composables'
-import useFetcher from '#cwa/runtime/server/useFetcher'
+import { resolveConfigEventHandler } from '#cwa/runtime/server/useFetcher'
 
 export default defineEventHandler(async (e) => {
-  const { mergeConfig, responseToConfig, resolvedConfigToSiteConfig } = useCwaSiteConfig()
-  const { fetcher, options } = useFetcher()
-  try {
-    const data = await fetcher<CwaResource>('/_/site_config_parameters')
-    const resolvedConfig = mergeConfig(options.siteConfig, responseToConfig(data, true))
+  const resolvedConfig = await resolveConfigEventHandler(e)
+  if (resolvedConfig) {
+    const { resolvedConfigToSiteConfig } = useCwaSiteConfig()
     updateSiteConfig(e, resolvedConfigToSiteConfig(resolvedConfig))
-  }
-  catch (e) {
-    consola.error(e)
-    return
   }
 })
--- src/runtime/server/useFetcher.ts
+++ src/runtime/server/useFetcher.ts
@@ -1,7 +1,11 @@
 import { $fetch } from 'ofetch'
+import { consola } from 'consola'
+import { defineEventHandler } from 'h3'
 import { useRuntimeConfig } from '#imports'
 // @ts-expect-error this is a file built in the module
 import { options } from '#cwa/server-options.ts'
+import useCwaSiteConfig from '#cwa/runtime/composables/useCwaSiteConfig'
+import type { CwaResource } from '#cwa/runtime/resources/resource-utils'
 
 export const useFetcher = () => {
   const { public: { cwa: { apiUrl, apiUrlBrowser } } } = useRuntimeConfig()
@@ -18,5 +22,18 @@ export const useFetcher = () => {
     options,
   }
 }
+
+export const resolveConfigEventHandler = defineEventHandler(async () => {
+  const { mergeConfig, responseToConfig } = useCwaSiteConfig()
+  const { fetcher, options } = useFetcher()
+  try {
+    const data = await fetcher<CwaResource>('/_/site_config_parameters')
+    return mergeConfig(options.siteConfig, responseToConfig(data, true))
+  }
+  catch (e) {
+    consola.error(e)
+    return
+  }
+})
 
 export default useFetcher
