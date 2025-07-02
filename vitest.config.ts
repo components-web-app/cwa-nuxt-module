7fc5bbb4abd4ea82ef62213967b13cd8e0e35bf2
silverbackdan <daniel@silverback.is>
2025-07-02 18:48:01 +0100

    Reinstall locally and latest deps - tests working

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
+    environment: 'nuxt',
     coverage: {
       reportsDirectory: '../coverage',
       provider: 'v8',
