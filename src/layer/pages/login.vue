84b87aeb2017646fea2bcaf5118b0cd3f26d12ea
silverbackdan <daniel@silverback.is>
2025-10-01 15:23:37 +0100

    remove admin login notice

--- src/layer/pages/login.vue
+++ src/layer/pages/login.vue
@@ -5,13 +5,7 @@
     :submitting="submitting"
     submit-button-text="Sign In"
     @submit="signIn"
-  >
-    <template #header>
-      <div class="py-2 px-3 border border-slate-600 bg-slate-900 rounded text-sm font-bold">
-        Use the username and password "admin" to log in to this preview version of CWA
-      </div>
-    </template>
-  </CwaAuthLoginPage>
+  />
 </template>
 
 <script setup lang="ts">
