b78bbb47a8736aa30a0497b5e15ef19db49e7f2d
silverbackdan <daniel@silverback.is>
2025-10-22 12:39:18 +0100

    Fix error status page messages

--- src/runtime/storage/stores/resources/actions.ts
+++ src/runtime/storage/stores/resources/actions.ts
@@ -8,6 +8,7 @@ import {
   isCwaResourceSame,
 } from '#cwa/resources/resource-utils'
 import type { CwaResource } from '#cwa/resources/resource-utils'
+import { createError } from 'h3'
 import type { CwaResourcesGettersInterface } from './getters'
 import type {
   CwaCurrentResourceInterface,
@@ -558,10 +559,18 @@ export default function (resourcesState: CwaResourcesStateInterface, resourcesGe
           consola.warn('-- SET COOKIE CALLED FROM ACTIONS -- ', parsedSetCookiesHeaders, currentSetCookieHeader.value, JSON.stringify(error?.asObject))
         }
 
+        const h3Error = createError<typeof error>({
+          name: 'cwa-resource-error',
+          statusCode: error.statusCode,
+          statusMessage: error.statusMessage,
+          message: error.statusMessage,
+          cause: 'Resource store returned a bad status code on a primary fetch',
+          data: error,
+        })
+        consola.info(h3Error)
         // , message: error.message - when the error related to a primary fetch of a resource - it's a bit verbose for
         // users to see this on the error page - especially for the 404 endpoint
-        showError({ statusCode: error.statusCode, statusMessage: error.statusMessage })
-        consola.info(error.message)
+        showError(h3Error)
       }
     },
     saveResource,
--- src/runtime/templates/components/main/ErrorPage.vue
+++ src/runtime/templates/components/main/ErrorPage.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import type { NuxtError } from 'nuxt/app'
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import { clearError, navigateTo, useHead, useRoute } from '#imports'
 import { useCwa } from '#cwa/composables/cwa'
 
@@ -33,8 +33,37 @@ const $route = useRoute()
 const $cwa = useCwa()
 
 const statusCode = Number(_error.statusCode || 500)
+
+const statusMessages: Record<number, string> = {
+  400: 'Bad Request',
+  401: 'Unauthorized',
+  403: 'Forbidden',
+  404: 'This Page Doesn’t Exist',
+  405: 'Method Not Allowed',
+  408: 'Request Timeout',
+  409: 'Conflict',
+  410: 'Gone',
+  418: 'I’m a Teapot ☕️',
+  429: 'Too Many Requests',
+  500: 'Internal Server Error',
+  501: 'Not Implemented',
+  502: 'Bad Gateway',
+  503: 'Service Unavailable',
+  504: 'Gateway Timeout',
+}
+
 const is404 = statusCode === 404
-const statusMessage = _error.statusMessage ?? (is404 ? 'Page Not Found' : 'Internal Server Error')
+const isAuthError = [401, 403].includes(statusCode)
+const statusMessage = computed(() => {
+  if (_error.statusMessage) {
+    return _error.statusMessage
+  }
+  if (statusMessages[statusCode]) {
+    return statusMessages[statusCode]
+  }
+  return 'Internal Server Error'
+})
+
 const description = _error.message || _error.toString()
 const stack = import.meta.dev && !is404 ? description || `${stacktrace}` : undefined
 
@@ -86,7 +115,7 @@ onMounted(() => {
               v-html="stack"
             />
           </div>
-          <div class="cwa:mt-10 cwa:flex cwa:items-center cwa:justify-center cwa:gap-x-6">
+          <div class="cwa:mt-10 cwa:flex cwa:flex-col cwa:items-center cwa:justify-center cwa:gap-y-6">
             <button
               v-if="$route.path !== '/'"
               class="cwa:cursor-pointer cwa:rounded-md cwa:bg-blue-600 cwa:px-3.5 cwa:py-2.5 cwa:text-sm cwa:font-semibold cwa:text-white cwa:shadow-sm cwa:hover:bg-indigo-500 cwa:focus-visible:outline cwa:focus-visible:outline-2 cwa:focus-visible:outline-offset-2 cwa:focus-visible:outline-indigo-600"
@@ -94,6 +123,13 @@ onMounted(() => {
             >
               Go back home
             </button>
+            <CwaLink
+              v-if="isAuthError"
+              to="/login"
+              class="cwa:cursor-pointer cwa:rounded-md cwa:bg-white/20 cwa:px-3.5 cwa:py-1.5 cwa:text-xs cwa:font-semibold cwa:text-white cwa:shadow-sm cwa:hover:bg-indigo-500 cwa:focus-visible:outline cwa:focus-visible:outline-2 cwa:focus-visible:outline-offset-2 cwa:focus-visible:outline-indigo-600"
+            >
+              Sign in
+            </CwaLink>
           </div>
         </div>
       </div>
