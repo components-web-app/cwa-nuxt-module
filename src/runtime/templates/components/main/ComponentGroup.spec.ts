03f819b24e875897098d94b335fcea30222561ab
Pavlo Khodakivskyi <pavlo_khodakivskyi@epam.com>
2026-04-22 21:51:17 +0300

    Fix ComponentGroup.spec.ts

--- src/runtime/templates/components/main/ComponentGroup.spec.ts
+++ src/runtime/templates/components/main/ComponentGroup.spec.ts
@@ -338,8 +338,7 @@ describe('ComponentGroup', () => {
       const watchSpy = vi.fn()
       const unwatchSpy = vi.fn()
 
-      // @ts-expect-error
-      ComponentGroupUtilSynchronizer.mockReturnValueOnce({ createSyncWatcher: watchSpy, stopSyncWatcher: unwatchSpy })
+      ComponentGroupUtilSynchronizer.mockImplementationOnce(function () { return { createSyncWatcher: watchSpy, stopSyncWatcher: unwatchSpy } })
 
       const wrapper = createWrapper()
       expect(watchSpy.mock.calls[0][0].resource.value).toEqual(wrapper.vm.resource)
