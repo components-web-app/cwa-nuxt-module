0454fcafa0663ee88e1396da608e061e349284c8
silverbackdan <daniel@silverback.is>
2026-04-22 16:05:49 +0100

    Reintrodiuce custom type files

---
+++ src/runtime/storage/stores/cwa-store-types.d.ts
@@ -0,0 +1,14 @@
+import type {
+  SetupStoreDefinition,
+  Store,
+  StoreActions,
+  StoreGetters,
+  StoreState,
+} from 'pinia'
+
+export interface CwaStore {
+  useStore(): any
+}
+
+export declare type CwaPiniaStoreDefinitionInterface<N extends string, I> = SetupStoreDefinition<N, I>
+export declare type CwaPiniaStoreInterface<N extends string, I> = Store<N, StoreState<I>, StoreGetters<I>, StoreActions<I>>
