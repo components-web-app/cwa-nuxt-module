49537e66dca1f682a9e03010d2b044c529d37383
silverbackdan <daniel@silverback.is>
2026-06-19 15:22:59 +0100

    Fix #204: publication status dot inline within title text
    Was rendered as a separate flex item alongside the title, appearing centred to the right of the text block when multi-line. Now nested as an inline-block span inside the title span so the dot flows at the end of the last line of text.

--- src/layer/pages/_cwa/index/data/[type].vue
+++ src/layer/pages/_cwa/index/data/[type].vue
@@ -25,9 +25,7 @@
     <template #item="{ data }">
       <div class="cwa:flex cwa:border-b cwa:border-b-stone-700 cwa:py-6 cwa:gap-x-4 cwa:items-center">
         <div class="cwa:grow cwa:flex cwa:flex-col cwa:gap-y-1">
-          <div class="cwa:flex cwa:items-end cwa:gap-x-3">
-            <span class="cwa:text-xl">{{ data.title }}</span><span :class="['cwa:outline', 'cwa:outline-1', 'cwa:outline-offset-2', 'cwa:w-2', 'cwa:h-2', 'cwa:shrink-0', 'cwa:rounded-full', data.route ? ['cwa:outline-green', 'cwa:bg-green'] : ['cwa:outline-orange', 'cwa:bg-orange']]" />
-          </div>
+          <span class="cwa:text-xl">{{ data.title }} <span :class="['cwa:inline-block', 'cwa:align-middle', 'cwa:outline', 'cwa:outline-1', 'cwa:outline-offset-2', 'cwa:w-2', 'cwa:h-2', 'cwa:shrink-0', 'cwa:rounded-full', data.route ? ['cwa:outline-green', 'cwa:bg-green'] : ['cwa:outline-orange', 'cwa:bg-orange']]" /></span>
           <span class="cwa:text-stone-400">{{ pageDataById[data.page]?.reference || data.page }}</span>
         </div>
         <div class="cwa:flex cwa:gap-x-2">
--- src/layer/pages/_cwa/index/pages.vue
+++ src/layer/pages/_cwa/index/pages.vue
@@ -45,9 +45,7 @@
     <template #item="{ data }">
       <div class="cwa:flex cwa:border-b cwa:border-b-stone-700 cwa:py-6 cwa:gap-x-4 cwa:items-center">
         <div class="cwa:grow cwa:flex cwa:flex-col cwa:gap-y-1">
-          <div class="cwa:flex cwa:items-end cwa:gap-x-3">
-            <span class="cwa:text-xl">{{ data.reference }}</span><span :class="['cwa:outline', 'cwa:outline-1', 'cwa:outline-offset-2', 'cwa:w-2', 'cwa:h-2', 'cwa:shrink-0', 'cwa:rounded-full', data.isTemplate ? 'cwa:outline-yellow cwa:bg-yellow' : 'cwa:outline-blue-600 cwa:bg-blue-600']" />
-          </div>
+          <span class="cwa:text-xl">{{ data.reference }} <span :class="['cwa:inline-block', 'cwa:align-middle', 'cwa:outline', 'cwa:outline-1', 'cwa:outline-offset-2', 'cwa:w-2', 'cwa:h-2', 'cwa:shrink-0', 'cwa:rounded-full', data.isTemplate ? 'cwa:outline-yellow cwa:bg-yellow' : 'cwa:outline-blue-600 cwa:bg-blue-600']" /></span>
           <span class="cwa:text-stone-400">UI: {{ data.uiComponent ? getDisplayPageUi(data.uiComponent) : 'Unknown' }}</span>
         </div>
         <div class="cwa:flex cwa:gap-x-2">
