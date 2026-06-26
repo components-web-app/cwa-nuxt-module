ba83aeb23ee3817f5bb8de8445b08d6431b74f81
silverbackdan <daniel@silverback.is>
2026-06-26 23:43:13 +0100

    Add useCwaLayout to secondary playground layout

--- playground/app/cwa/layouts/secondary.vue
+++ playground/app/cwa/layouts/secondary.vue
@@ -13,9 +13,10 @@
 
 <script setup lang="ts">
 import { computed } from 'vue'
-import { useCwa } from '#imports'
+import { useCwa, useCwaLayout } from '#imports'
 
 const $cwa = useCwa()
+useCwaLayout()
 
 const percent = computed(() => $cwa.resources.pageLoadProgress.value.percent || 3)
 const showPageLoadBar = computed(() => percent.value < 100)
